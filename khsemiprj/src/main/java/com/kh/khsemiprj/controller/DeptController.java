7cc598b984858b09f10561953f43f803e6c407f2
Shyael <seokkid@gmail.com>
2026-06-01 17:28:56 +0900

    부서 조직도 작업

--- khsemiprj/src/main/java/com/kh/khsemiprj/controller/DeptController.java
+++ khsemiprj/src/main/java/com/kh/khsemiprj/controller/DeptController.java
@@ -1,6 +1,9 @@
 package com.kh.khsemiprj.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +11,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kh.khsemiprj.dao.DeptDao;
 import com.kh.khsemiprj.dto.DeptDto;
 
@@ -30,9 +35,39 @@ public class DeptController {
 	}
 	
 	@RequestMapping("/chart")
-	public String chart(Model model) {
+	public String chart(Model model) throws JsonProcessingException {
+		
+		// 1. 부서 목록 가져오기
  		List<DeptDto> list = deptDao.selectListAll();
- 		model.addAttribute("list", list);
+ 		
+ 		// 2. 부서 목록 트리구조로 변경
+ 		List<DeptDto> rootList = new ArrayList<>();
+ 	    Map<Long, DeptDto> dtoMap = new HashMap<>();
+ 	    
+ 	    // - 2-1. Map에 모두 저장
+ 	    for (DeptDto dto : list) {
+ 	        dtoMap.put(dto.getDeptNo(), dto);
+ 	    }
+ 	    
+ 	    // - 2-2. 부서번호를 키값으로 가지는 해시맵 생성
+ 	    for (DeptDto dto : list) {
+ 	    	Long deptParentNo = dto.getDeptParentNo();
+ 	    	dtoMap.put(dto.getDeptNo(), dto);
+ 	    	// 부모 ID가 없거나, 부모 ID가 있지만 Map에 존재하지 않는 경우 최상위(Root)로 취급
+ 	    	if (dto.getDeptDepth() == 0 || dto.getDeptParentNo() == null || !dtoMap.containsKey(deptParentNo)) {
+ 	            rootList.add(dto);
+ 	        } else {
+ 	        	// 부모가 있다면 해당 부모의 자식 리스트에 추가
+ 	            dtoMap.get(deptParentNo).getChildren().add(dto);
+ 	        }
+ 	    }
+ 		
+ 	    // 3. 자바 객체를 JSP의 JavaScript가 인식할 수 있도록 JSON 문자열로 변환
+ 	    ObjectMapper objectMapper = new ObjectMapper();
+ 	    String deptChartJson = objectMapper.writeValueAsString(rootList);
+ 	    
+ 	    // 4. Model에 담아서 jsp로 전달
+ 		model.addAttribute("deptChartJson", deptChartJson);
 		
 		return "dept/chart";
 	}
