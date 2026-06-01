74b962695cd8cabf800e3aa63d5da53ab6322b2a
Shyael <seokkid@gmail.com>
2026-06-01 15:26:37 +0900

    부서 작업중

--- khsemiprj/src/main/java/com/kh/khsemiprj/dao/DeptDao.java
+++ khsemiprj/src/main/java/com/kh/khsemiprj/dao/DeptDao.java
@@ -1,9 +1,13 @@
 package com.kh.khsemiprj.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.kh.khsemiprj.dto.DeptDto;
+import com.kh.khsemiprj.dto.EmpDto;
 import com.kh.khsemiprj.mapper.DeptMapper;
 
 @Repository
@@ -13,5 +17,9 @@ public class DeptDao {
 	@Autowired
 	private DeptMapper deptMapper;
 	
-	
+	public List<DeptDto> selectListAll() {
+		String sql = "select * from dept where dept_use_yn = 'Y'";
+		Object[] params = {  };
+		return jdbcTemplate.query(sql, deptMapper, params);
+	}
 }
