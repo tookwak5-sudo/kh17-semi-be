6bb79a574b633f43e5456062df55fecd19bbe5d2
Ojinchub <danykim05@gmail.com>
2026-06-04 17:34:37 +0900

    예외처리 구현중

--- khsemiprj/src/main/java/com/kh/khsemiprj/aop/AdminOnlyInterceptor.java
+++ khsemiprj/src/main/java/com/kh/khsemiprj/aop/AdminOnlyInterceptor.java
@@ -1,29 +1,31 @@
 package com.kh.khsemiprj.aop;
 
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import com.kh.khsemiprj.exception.GetOutException;
 import com.kh.khsemiprj.exception.WhoAreYouException;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-@Service
-public class EmpOnlyInterceptor implements HandlerInterceptor {
+@Component
+public class AdminOnlyInterceptor implements HandlerInterceptor{
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 		throws Exception {
+		
 		HttpSession session = request.getSession();
 		
-		String loginId = (String) session.getAttribute("loginId");
 		Integer empGrade = (Integer) session.getAttribute("empGrade");
 		
+		//비회원?
+		if(empGrade == null) throw new WhoAreYouException();
 		
-		if(loginId == null || empGrade == null) {
-			throw new WhoAreYouException();
-		}
+		//부서장 이상인지
+		if(empGrade > 1) throw new GetOutException();
 		
-		return true;//통과!
+		return true;
 	}
 }
